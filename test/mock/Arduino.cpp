3eaf42eb0bafae498eefdff051b685cd21379210
offa <bm-dev@yandex.com>
2021-01-08 14:43:32 +0100

    Year updated.

--- test/mock/Arduino.cpp
+++ test/mock/Arduino.cpp
@@ -1,7 +1,7 @@
 /*
  * MIDI Footswitch
  *
- * Copyright (C) 2019-2020  offa
+ * Copyright (C) 2019-2021  offa
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
