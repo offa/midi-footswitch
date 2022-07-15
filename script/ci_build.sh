d34f5c1fcc7af1532f32d3a98bf89f7a4bced55d
offa <bm-dev@yandex.com>
2022-07-15 16:02:41 +0200

    Update Conan profile

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -11,13 +11,16 @@ conan profile new default --detect
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
+    conan profile update settings.compiler.libcxx=libc++ default
 else
     conan profile update settings.compiler.libcxx=libstdc++11 default
 fi
 
+conan profile update settings.compiler.cppstd=17 default
+
 mkdir build && cd build
 
-conan install --build=missing -s compiler.cppstd=17 ..
+conan install --build=missing ..
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=conan_paths.cmake ..
 make
