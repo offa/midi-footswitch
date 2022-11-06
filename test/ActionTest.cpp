dabccc859ef9a2f599d0407c2c2ac3286d68b2e6
offa <bm-dev@yandex.com>
2022-11-06 15:20:10 +0100

    Update Trompeloeil v43

--- test/ActionTest.cpp
+++ test/ActionTest.cpp
@@ -20,7 +20,6 @@
 #include "MidiButton.h"
 #include "mock/Mocks.h"
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/catch_version_macros.hpp>
 #include <catch2/trompeloeil.hpp>
 
 namespace
