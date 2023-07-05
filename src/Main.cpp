ee0bf326ce27e6d95efed1623364af012b863fea
offa <bm-dev@yandex.com>
2023-07-05 19:48:13 +0200

    Update year

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,7 @@
 /*
  * MIDI Footswitch
  *
- * Copyright (C) 2019-2022  offa
+ * Copyright (C) 2019-2023  offa
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
